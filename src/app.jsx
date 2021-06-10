311d11919f3b23a1aa69c341f21c10436fce8ccf
youngjun114 <35579719+youngjun114@users.noreply.github.com>
2021-06-10 00:22:25 -0700

    firebase authentication setup

--- src/app.jsx
+++ src/app.jsx
@@ -2,35 +2,27 @@ import React from 'react';
 import './app.module.css';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import * as firebaseui from 'firebaseui';
-import 'firebaseui/dist/firebaseui.css';
 
 function App() {
-  const provider = new firebaseui.auth.GoogleAuthProvider();
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: 'business-car-maker.firebaseapp.com',
+    databaseURL: 'https://business-car-maker-default-rtdb.firebaseio.com',
+    projectId: 'business-car-maker',
+    storageBucket: 'business-car-maker.appspot.com',
+    messagingSenderId: '423929136510',
+    appId: '1:423929136510:web:bc7346909a01a7392edf2f',
+    measurementId: 'G-8JJRRNXWEF',
+  };
 
-  firebase
-    .auth()
-    .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      const credential = result.credential;
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // ...
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      const credential = error.credential;
-      // ...
-    });
-  return <></>;
+  return (
+    <>
+      <FirebaseAuthProvider
+        firebase={firebase}
+        {...firebaseConfig}
+      ></FirebaseAuthProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/service/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+firebase.intializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebaseConfig;
