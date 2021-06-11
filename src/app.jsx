95b92fab1e32b513e622a38e71b583c5e07d71cb
youngjun114 <35579719+youngjun114@users.noreply.github.com>
2021-06-10 23:49:01 -0700

    added login page

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import './app.module.css';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import styles from './app.module.css';
+import Login from './components/login/login';
 
-function App() {
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: 'business-car-maker.firebaseapp.com',
-    databaseURL: 'https://business-car-maker-default-rtdb.firebaseio.com',
-    projectId: 'business-car-maker',
-    storageBucket: 'business-car-maker.appspot.com',
-    messagingSenderId: '423929136510',
-    appId: '1:423929136510:web:bc7346909a01a7392edf2f',
-    measurementId: 'G-8JJRRNXWEF',
-  };
-
+function App({ authService }) {
   return (
-    <>
-      <FirebaseAuthProvider
-        firebase={firebase}
-        {...firebaseConfig}
-      ></FirebaseAuthProvider>
-    </>
+    <div className={styles.app}>
+      <Login authService={authService} />
+    </div>
   );
 }
 
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,10 +1,12 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
-firebase.intializeApp(firebaseConfig);
-firebase.analytics();
 
-export default firebaseConfig;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp;
